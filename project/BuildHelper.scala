27bbe9c9a4f8444f04026549a1453f71f6a9b915
Scala Steward <scala_steward@virtuslab.com>
2025-06-14 08:49:42 +0000

    Update scala3-compiler, scala3-library, ... to 3.3.6

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -14,7 +14,7 @@ object BuildHelper {
 
   final val Scala212 = "2.12.20"
   final val Scala213 = "2.13.16"
-  final val Scala3   = "3.3.3" // LTS
+  final val Scala3   = "3.3.6" // LTS
 
   final private val stdOptions = Seq(
     "-deprecation",
